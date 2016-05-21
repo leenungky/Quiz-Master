30a76544dc25e8c28800d42504b8794f90d8eae0
nungky <lee.nungky@gmail.com>
2016-05-21 16:02:51 +0700

    a

--- blog/test/models/post_test.rb
+++ blog/test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true;
+     assert true;
+   end
 end
